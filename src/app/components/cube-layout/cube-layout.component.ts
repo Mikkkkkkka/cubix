d2579da3420b0af4f075ac478c19bfbb71589611
Mikhail Kashin <144782206+Mikkkkkkka@users.noreply.github.com>
2026-04-10 01:26:51 +0300

    ref: the cube-layout.component now uses signals instead of directly mutating the private fields

--- src/app/components/cube-layout/cube-layout.component.ts
+++ src/app/components/cube-layout/cube-layout.component.ts
@@ -1,4 +1,4 @@
-import {Component} from '@angular/core';
+import {Component, computed, signal} from '@angular/core';
 import {CubeSquare, SquareColor} from '../../models/cube-square';
 
 @Component({
@@ -8,98 +8,63 @@ import {CubeSquare, SquareColor} from '../../models/cube-square';
   styleUrl: './cube-layout.component.css',
 })
 export class CubeLayoutComponent {
-  private readonly _topFace: CubeSquare[][];
-  private readonly _leftFace: CubeSquare[][];
-  private readonly _frontFace: CubeSquare[][];
-  private readonly _rightFace: CubeSquare[][];
-  private readonly _backFace: CubeSquare[][];
-  private readonly _bottomFace: CubeSquare[][];
-  private size: number = 3;
+  private readonly size = 3;
+  private readonly topFace = signal(this.generateFace(SquareColor.WHITE));
+  private readonly leftFace = signal(this.generateFace(SquareColor.ORANGE));
+  private readonly frontFace = signal(this.generateFace(SquareColor.GREEN));
+  private readonly rightFace = signal(this.generateFace(SquareColor.RED));
+  private readonly backFace = signal(this.generateFace(SquareColor.BLUE));
+  private readonly bottomFace = signal(this.generateFace(SquareColor.YELLOW));
 
-  constructor() {
-    this._topFace = this.generateFace(SquareColor.WHITE);
-    this._leftFace = this.generateFace(SquareColor.ORANGE);
-    this._frontFace = this.generateFace(SquareColor.GREEN);
-    this._rightFace = this.generateFace(SquareColor.RED);
-    this._backFace = this.generateFace(SquareColor.BLUE);
-    this._bottomFace = this.generateFace(SquareColor.YELLOW);
-  }
-
-  get topFace(): CubeSquare[][] {
-    return [...this._topFace];
-  }
-
-  get leftFace(): CubeSquare[][] {
-    return [...this._leftFace];
-  }
-
-  get frontFace(): CubeSquare[][] {
-    return [...this._frontFace];
-  }
-
-  get rightFace(): CubeSquare[][] {
-    return [...this._rightFace];
-  }
-
-  get backFace(): CubeSquare[][] {
-    return [...this._backFace];
-  }
-
-  get bottomFace(): CubeSquare[][] {
-    return [...this._bottomFace];
-  }
-
-  get faces() {
-    return [
-      { name: 'top', face: this.topFace, move: () => this.u() },
-      { name: 'left', face: this.leftFace, move: null },
-      { name: 'front', face: this.frontFace, move: () => this.f() },
-      { name: 'right', face: this.rightFace, move: () => this.r() },
-      { name: 'back', face: this.backFace, move: null },
-      { name: 'bottom', face: this.bottomFace, move: null },
-    ];
-  }
+  readonly faces = computed(() => [
+    { name: 'top', face: this.topFace(), move: () => this.u() },
+    { name: 'left', face: this.leftFace(), move: null },
+    { name: 'front', face: this.frontFace(), move: () => this.f() },
+    { name: 'right', face: this.rightFace(), move: () => this.r() },
+    { name: 'back', face: this.backFace(), move: null },
+    { name: 'bottom', face: this.bottomFace(), move: null },
+  ]);
 
   public u() {
-    this.rotateFaceClockwise(this._topFace);
+    this.topFace.update((face) => this.rotateFaceClockwise(face));
 
-    const front = this.getRow(this._frontFace, 0);
-    const right = this.getRow(this._rightFace, 0);
-    const back = this.getRow(this._backFace, 0);
-    const left = this.getRow(this._leftFace, 0);
+    const front = this.getRow(this.frontFace(), 0);
+    const right = this.getRow(this.rightFace(), 0);
+    const back = this.getRow(this.backFace(), 0);
+    const left = this.getRow(this.leftFace(), 0);
 
-    this.setRow(this._frontFace, 0, right);
-    this.setRow(this._leftFace, 0, front);
-    this.setRow(this._backFace, 0, left);
-    this.setRow(this._rightFace, 0, back);
+    this.frontFace.update((face) => this.setRow(face, 0, right));
+    this.leftFace.update((face) => this.setRow(face, 0, front));
+    this.backFace.update((face) => this.setRow(face, 0, left));
+    this.rightFace.update((face) => this.setRow(face, 0, back));
   }
 
   public r() {
-    this.rotateFaceClockwise(this._rightFace);
+    this.rightFace.update((face) => this.rotateFaceClockwise(face));
 
-    const top = this.getColumn(this._topFace, this.size - 1);
-    const front = this.getColumn(this._frontFace, this.size - 1);
-    const bottom = this.getColumn(this._bottomFace, this.size - 1);
-    const back = this.getColumn(this._backFace, 0);
+    const top = this.getColumn(this.topFace(), this.size - 1);
+    const front = this.getColumn(this.frontFace(), this.size - 1);
+    const bottom = this.getColumn(this.bottomFace(), this.size - 1);
+    const back = this.getColumn(this.backFace(), 0);
 
-    this.setColumn(this._frontFace, this.size - 1, top);
-    this.setColumn(this._bottomFace, this.size - 1, front);
-    this.setColumn(this._backFace, 0, [...bottom].reverse());
-    this.setColumn(this._topFace, this.size - 1, [...back].reverse());
+    this.frontFace.update((face) => this.setColumn(face, this.size - 1, top));
+    this.bottomFace.update((face) => this.setColumn(face, this.size - 1, front));
+    this.backFace.update((face) => this.setColumn(face, 0, [...bottom].reverse()));
+    this.topFace.update((face) => this.setColumn(face, this.size - 1, [...back].reverse()));
   }
 
   public f() {
-    this.rotateFaceClockwise(this._frontFace);
+    this.frontFace.update((face) => this.rotateFaceClockwise(face));
 
-    const top = this.getRow(this._topFace, this.size - 1);
-    const right = this.getColumn(this._rightFace, 0);
-    const bottom = this.getRow(this._bottomFace, 0);
-    const left = this.getColumn(this._leftFace, this.size - 1);
+    const top = this.getRow(this.topFace(), this.size - 1);
+    const right = this.getColumn(this.rightFace(), 0);
+    const bottom = this.getRow(this.bottomFace(), 0);
+    const left = this.getColumn(this.leftFace(), this.size - 1);
 
-    this.setColumn(this._rightFace, 0, top);
-    this.setRow(this._bottomFace, 0, [...right].reverse());
-    this.setColumn(this._leftFace, this.size - 1, bottom);
-    this.setRow(this._topFace, this.size - 1, [...left].reverse());
+    this.rightFace.update((face) => this.setColumn(face, 0, top));
+    this.bottomFace.update((face) => this.setRow(face, 0, [...right].reverse()));
+    this.leftFace.update((face) => this.setColumn(face, this.size - 1, bottom));
+    this.topFace.update((face) => this.setRow(face, this.size - 1, [...left].reverse()));
   }
 
   private generateFace(color: SquareColor): CubeSquare[][] {
@@ -113,33 +78,23 @@ export class CubeLayoutComponent {
     return squares;
   }
 
-  private rotateFaceClockwise(face: CubeSquare[][]): void {
-    const rotated = face.map((_, rowIndex) =>
-      face.map((row) => row[rowIndex]).reverse(),
-    );
-
-    for (let row = 0; row < this.size; row++) {
-      for (let column = 0; column < this.size; column++) {
-        face[row][column] = rotated[row][column];
-      }
-    }
+  private rotateFaceClockwise(face: CubeSquare[][]): CubeSquare[][] {
+    return face.map((_, rowIndex) => face.map((row) => row[rowIndex]).reverse(),);
   }
 
   private getRow(face: CubeSquare[][], rowIndex: number): CubeSquare[] {
     return [...face[rowIndex]];
   }
 
-  private setRow(face: CubeSquare[][], rowIndex: number, values: CubeSquare[]): void {
-    face[rowIndex] = [...values];
+  private setRow(face: CubeSquare[][], rowIndex: number, values: CubeSquare[]): CubeSquare[][] {
+    return face.map((row, index) => index === rowIndex ? [...values] : [...row]);
   }
 
   private getColumn(face: CubeSquare[][], columnIndex: number): CubeSquare[] {
     return face.map((row) => row[columnIndex]);
   }
 
-  private setColumn(face: CubeSquare[][], columnIndex: number, values: CubeSquare[]): void {
-    values.forEach((value, rowIndex) => {
-      face[rowIndex][columnIndex] = value;
-    });
+  private setColumn(face: CubeSquare[][], columnIndex: number, values: CubeSquare[]): CubeSquare[][] {
+    return face.map((row, rowIndex) => row.map((square, squareIndex) => squareIndex === columnIndex ? values[rowIndex] : square),);
   }
 }
